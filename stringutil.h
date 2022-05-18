19a9eee653bff0b71843fea6929dfe4b68b76e3f
liuzehao <1437070389@qq.com>
2022-05-18 17:20:48 +0800

    数组to_string函数头文件

---
+++ stringutil.h
@@ -0,0 +1,12 @@
+#include <malloc.h>
+
+
+#include <stdio.h>
+#include <malloc.h>
+
+char* int_array_to_string(int*, int, char);
+// array of integer string size
+int static size_table[] = {9, 99, 999, 9999, 99999, 999999, 9999999, 99999999, 999999999, 0x7FFFFFFF};
+
+// get the string of the int parameter
+unsigned string_size(int);
